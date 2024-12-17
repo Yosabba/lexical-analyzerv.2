f9ab46b9e1b75210d1094d715d5a51bfc7033ea1
DomJohn <85464248+Yosabba@users.noreply.github.com>
2024-12-16 20:44:10 -0500

    added new function for writing output to files. renamed fileName var

--- main.go
+++ main.go
@@ -21,36 +21,55 @@ type Token struct {
 
 func readUserFile(fileName string) string {
 	file, err := os.Open(fileName)
-	if err != nil {
+	if (err != nil) {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
 	content, err := os.ReadFile(fileName)
-	if err != nil {
+	if (err != nil) {
 		log.Fatal(err)
 	}
 
 	return string(content)
 }
 
+func writeTokensToFile(tokens []Token, fileName string) {
+	file, err := os.Create(fileName)
+	if (err != nil) {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	for _, token := range tokens {
+		_, err := file.WriteString(fmt.Sprintf("%-10s %-10s\n", token.Lexeme, token.Type))
+		if (err != nil) {
+			log.Fatal(err)
+		}
+	}
+}
+
 func main() {
-	var fileName string
+	var inputFileName, outputFileName string
 
-	fmt.Println("What is the name of your file:?\nPlease add the extension (i.e: example.txt)")
-	fmt.Scanln(&fileName)
+	fmt.Println("What is the name of your input file?\nPlease add the extension (i.e: example.txt)")
+	fmt.Scanln(&inputFileName)
 
-	contentOfFile := readUserFile(fileName)
+	contentOfFile := readUserFile(inputFileName)
 	tokens := tokenizeFile(contentOfFile)
 
-	//lexeme & token needs to be be in a table so format it to be in a table with a 10 char min
+	fmt.Println("What do you want to name your output file?\nPlease add the extension of file you want also. (i.e: example.txt, example.docx)")
+	fmt.Scanln(&outputFileName)
+
+	writeTokensToFile(tokens, outputFileName)
+
+	// Print tokens to console
 	fmt.Printf("%-10s %-10s\n", "Lexeme", "Token")
 	fmt.Println(strings.Repeat("-", 20))
 
 	for _, token := range tokens {
 		fmt.Printf("%-10s %-10s\n", token.Lexeme, token.Type)
 	}
-
 }
 
 func isCharOperator(character rune) bool {
